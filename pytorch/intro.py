d5ce53d3c0ad5654602120aac7929215068626ba
Riri Ianos <riri@mac.mynetworksettings.com>
2025-07-15 16:24:19 -0400

    v3

---
+++ pytorch/intro.py
@@ -0,0 +1,4 @@
+
+import torch #import the torch package, download with pip install torch (go to https://pytorch.org/get-started/locally/ for the CUDA version)
+x = torch.rand(5, 3) #create a 5*3 matrix, first number is number of rows, second number is number of columns, rand = random numbers generated
+print(x) #print the x value
